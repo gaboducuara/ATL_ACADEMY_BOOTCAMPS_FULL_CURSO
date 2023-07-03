b4f8ea9938ee31a4dcc576b3cebcf464bacd1103
gabriel <gabrielmancilladucuara@gmail.com>
2023-07-03 16:19:26 -0500

    corrigiendo archivos

---
+++ programacion/practicas/Variables.java
@@ -0,0 +1,45 @@
+
+
+//1) Declara dos variables numéricas (con el valor que desees),
+// muestra por consola la suma, resta, multiplicación, división
+// y módulo (resto de la división).
+public class Variables {
+  /*  public static void main (String[] args) {
+        int num1 = 100;
+        int num2 = 200;
+        int num3 = 2;
+        int num4 = 5;
+        int num5 = 10;
+
+        System.out.println("El resultado de la resta es:" + " " + (num2 - num5));
+        System.out.println("El resultado de la suma es:" + " " + (num1 + num2));
+        System.out.println("El resultado de la multiplicacion es:" + " " + (num1 + num4));
+        System.out.println("El resultado de la division es:" + " " + (num2 / num3));
+    } */
+
+    // Declare dos variables numericas, he indique, cual de los dos es mayor
+    // de los dos. Si son iguales indicarlo tambien. Vas cambiando los valores
+    // para comprobar que funciona.
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 200;
+        int num3 = 2;
+        int num4 = 5;
+        int num5 = 10;
+        int num6 = 11;
+        int num7 = 12;
+        int num10 = 1;
+        int num11 = 1;
+
+        if (num1 > num11) {
+            System.out.println(" si num1 es mayor a num11");
+        } else if (num11 == num10) ;
+
+                    
+                {
+            }
+        }
+    }
+}
+
+
